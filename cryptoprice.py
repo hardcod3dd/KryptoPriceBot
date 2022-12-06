0dc812630ef66c8cf61bc735284a9d3524ed0b42
hardcod3d <23269636+hardcod3dd@users.noreply.github.com>
2022-12-06 08:51:48 +0300

    v1

---
+++ cryptoprice.py
@@ -0,0 +1,38 @@
+import tweepy
+import time
+import json
+import requests
+
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
+api = tweepy.API(auth)
+cryptos = ['BTC', 'ETH', 'BNB','XRP', 'DOGE', 'AVAX', 'SOL', 'DOT']
+prices = {}
+url = 'https://api.binance.com/api/v3/ticker/price?symbol={}USDT'
+
+def getprices():
+    for crypto in cryptos:
+        response = requests.get(url.format(crypto))
+        if response.status_code == 200:
+            price = float(response.json()['price'])
+            prices[crypto] = price
+    
+def tweet():
+    getprices()
+    status = (f"#BTC Bitcoin ${prices.get('BTC'):,}\n#ETH Ethereum ${prices.get('ETH'):,}\n#BNB BNB ${prices.get('BNB'):,}\n#XRP Ripple ${prices.get('XRP'):,}\n#DOGE Dogecoin ${prices.get('DOGE'):,}")
+    api.update_status(status)
+def main():
+    while True:
+        try:
+            tweet()
+            print("tweeted")
+            time.sleep(30 * 60)
+        except KeyboardInterrupt:
+            break
+        except:
+            print("some error.")
+            pass
+
+
+if __name__ == "__main__":
+    main()
